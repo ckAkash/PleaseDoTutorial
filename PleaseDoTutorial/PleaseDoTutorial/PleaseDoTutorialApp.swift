0ced5780f706a17d4d5467698a611a21e2f01ed2
ckAkash <akash.yadav@cloudkaptan.com>
2025-08-21 11:40:13 +0530

    Project Setup 1

---
+++ PleaseDoTutorial/PleaseDoTutorial/PleaseDoTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  PleaseDoTutorialApp.swift
+//  PleaseDoTutorial
+//
+//  Created by Akash Yadav on 21/08/25.
+//
+
+import SwiftUI
+
+@main
+struct PleaseDoTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PleaseDoTutorial/PleaseDoTutorialTests/PleaseDoTutorialTests.swift
@@ -0,0 +1,17 @@
+//
+//  PleaseDoTutorialTests.swift
+//  PleaseDoTutorialTests
+//
+//  Created by Akash Yadav on 21/08/25.
+//
+
+import Testing
+@testable import PleaseDoTutorial
+
+struct PleaseDoTutorialTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
